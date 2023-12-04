be572444c63760ce5d68c35e2bf32ab05af045ff
MarineniGnanasai <marinenignanasai@gmail.com>
2023-12-04 18:13:28 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# crypto-dashboard-internship
- 
+STEPS TO RUN THE PROJECT:
+STEP 1: npm install
+STEP 2: npm start
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Header = () => {
+  return (
+    <div className="text-center">
+      <h1 className="text-4xl font-bold text-white">Crypto Dashboard💹</h1>
+    </div>
+  );
+};
